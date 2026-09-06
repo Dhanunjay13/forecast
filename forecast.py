d97e27a88574256f0bdcae33f9f8853ef2814565
Basani Dhanunjay <dhanunjaybasani@gmail.com>
2026-09-06 19:30:12 +0530

    Update forecast.py

--- forecast.py
+++ forecast.py
@@ -7,8 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/1MoOXZh-26BCaBT4U_lwdAv5TglNQdLIZ
 """
 
-!pip install Streamlit
-
 import streamlit as st
 import pandas as pd
 import numpy as np
